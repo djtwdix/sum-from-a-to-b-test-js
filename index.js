cec2d61785ac7e28139f6dead3e44ad7685988b7
Daniel Tweedie <djtwd.ix@icloud.com>
2021-02-06 13:47:26 +0000

    Function that takes in smaller number and larger number, sums all number from smaller to larger using recursion

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (fromN === toN) {
+    return fromN;
+  }
+  if (fromN <= toN) {
+    return fromN + sum(fromN + 1, toN);
+  }
+};
+
+console.log(sum(3,7));
 
 module.exports = sum;
